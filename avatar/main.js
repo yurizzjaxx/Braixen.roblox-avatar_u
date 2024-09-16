082325bb8cf2c9225e2e0a8fd829705a8c4f43af
yuriZz3ddy <132253210+yuriZz3ddy@users.noreply.github.com>
2024-09-16 01:07:15 -0400

    Update main.js

--- avatar/main.js
+++ avatar/main.js
@@ -1,8 +1,11 @@
 document.title = "Roblox Avatar";
 let text1;
+var onCrate = location.search;
+var urlCode = new URLSearchParams(onCrate)
+var userId = urlCode.get("userId")
 
 function main() {
-fetch("https://avatar.roblox.com/v1/users/" + userId + "/avatar", { 
+async = fetch("https://avatar.roblox.com/v1/users/" + userId + "/avatar", { 
 method: "GET",
   headers: {
     "Content-Type": "application/json"
@@ -10,7 +13,11 @@ method: "GET",
 })
 .then(res => res.json())
 .then(data => {
-
+var onText = data
+text1 = document.getElementById("_01") 
+  text1.innerHTML = `
+<p>${onText}</p>
+  `
 })
 .catch(er => {
 console.error(er)
