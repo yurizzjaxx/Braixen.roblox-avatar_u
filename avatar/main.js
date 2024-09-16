b9601af5eb714b873f819be53e98b525accea3a7
yuriZz3ddy <132253210+yuriZz3ddy@users.noreply.github.com>
2024-09-16 00:53:38 -0400

    Update main.js

--- avatar/main.js
+++ avatar/main.js
@@ -1,1 +1,21 @@
 document.title = "Roblox Avatar";
+let text1;
+
+function main() {
+fetch("https://avatar.roblox.com/v1/users/" + userId + "/avatar", { 
+method: "GET",
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+.then(res => res.json())
+.then(data => {
+
+})
+.catch(er => {
+console.error(er)
+});
+
+
+}
+main()
