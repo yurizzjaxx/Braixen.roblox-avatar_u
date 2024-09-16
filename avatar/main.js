0fb2eb322e3c22b3becec27b75f95e2aa79d0c77
yuriZz3ddy <132253210+yuriZz3ddy@users.noreply.github.com>
2024-09-16 01:28:19 -0400

    Update main.js

--- avatar/main.js
+++ avatar/main.js
@@ -4,25 +4,24 @@ var onCrate = location.search;
 var urlCode = new URLSearchParams(onCrate)
 var userId = urlCode.get("userId")
 
-function main() {
-async = fetch("https://avatar.roblox.com/v1/users/" + userId + "/avatar", { 
-method: "GET",
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
-.then(res => res.json())
-.then(data => {
-var onText = data
-text1 = document.getElementById("_01") 
-  text1.innerHTML = `
-<p>${onText}</p>
-  `
-})
+function main(userId) {
+
+async = fetch("https://avatar.roblox.com/v1/users/" + userId + "/avatar", {
+	method: "GET"
+	})
+	.then(res => {
+	return res.json()
+	})
+	.then(data => {
+    var onText = data
+		text1 = document.getElementById("_01") 
+    text1.innerHTML = `
+    <p>${onText}</p>
+    `
+  )}
 .catch(er => {
 console.error(er)
 });
-
 
 }
 main()
